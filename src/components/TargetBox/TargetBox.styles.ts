19aae8759c15335cdf894744cb261ec89905d728
J Heaton <jackrobertheaton@gmail.com>
2022-01-28 14:30:11 -0500

    Create and add main game image. Add character icons

--- src/components/TargetBox/TargetBox.styles.ts
+++ src/components/TargetBox/TargetBox.styles.ts
@@ -4,7 +4,7 @@ import arrow from "../../assets/arrow.png";
 
 export const TargetBox = styled.div`
   position: absolute;
-  top: ${ (props: ITargetBoxStyledProps) =>  (props.coords.yPos + 15) + "px"};
+  top: ${ (props: ITargetBoxStyledProps) =>  (props.coords.yPos - 40) + "px"};
   left: ${ (props: ITargetBoxStyledProps) =>  (props.coords.xPos - 50) + "px"};
   display: flex;
   flex-direction: row;
@@ -30,30 +30,45 @@ export const TargetBox = styled.div`
 
 export const Dropdown = styled.div`
   width: 200px;
-  height: 350px;
+  height: auto;
+  min-height: 375px;
   background: linear-gradient(to bottom, rgb(62, 81, 81), rgb(222, 203, 164));
   font-family: Baskerville, sans-serif;
   text-align: center;
   margin-left: 10px;
   border-radius: 1em;
+  
+  .dropDownHeader {
+    font-size: 2em;
+    margin: 5px 0;
+    color: goldenrod;
+  }
 
   ul {
     width: 100%;
     list-style: none;
+    margin: 5px 0 0 0;
     padding: 0;
   }
-
 `
 
 export const LiStyled = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
   width: 100%;
   color: #FFF;
   font-size: 1.5em;
   letter-spacing: 1px;
-  padding: 10px 0;
+  padding: 15px 0;
   cursor: pointer;
   text-decoration: ${ (props: IListStyledProps) =>  props.found ? "line-through" : "none"};
 
+  img {
+    width: 75px;
+    height: 75px;
+    border-radius: 5px;
+  }
 
   :hover {
     font-size: 1.7em;
--- src/components/TargetBox/TargetBox.tsx
+++ src/components/TargetBox/TargetBox.tsx
@@ -1,7 +1,19 @@
+// React
 import React, {useContext} from 'react';
-import * as S from "./TargetBox.styles"
+
+// Types
 import {IMousePos, IPerson, ITargetBoxProps} from "../../types/Main.types";
+
+// Context & Styles
 import { PhotoContext } from "../../App";
+import * as S from "./TargetBox.styles";
+
+// Character Images
+import Thoreau from "../../assets/characterIcons/Thoreau.jpeg"
+import Waldo from "../../assets/characterIcons/Waldo.jpeg"
+import Whitman from "../../assets/characterIcons/Whitman.jpeg"
+
+
 const TargetBox: React.FC<ITargetBoxProps> = ( {mouseCoords, relativeCoords, tryAgain} ) => {
 
     const  {photos, dispatch} = useContext(PhotoContext)
@@ -18,10 +30,11 @@ const TargetBox: React.FC<ITargetBoxProps> = ( {mouseCoords, relativeCoords, try
             }
 
             const checkBox = (trueCoords: IMousePos , i: number) => {
-                const xRange = [relativeCoords.xPos - 2, relativeCoords.xPos + 2]
-                const yRange = [relativeCoords.yPos - 2, relativeCoords.yPos + 2]
+                // Check if character true position falls in range of 100px of selected mouse pos.
+                const xRange = [relativeCoords.xPos - 50, relativeCoords.xPos + 50]
+                const yRange = [relativeCoords.yPos - 50, relativeCoords.yPos + 50]
 
-                const checkTrue = () => {
+                const checkTrue = (): void => {
                     if (trueCoords.xPos >= xRange[0] && trueCoords.xPos <= xRange[1]) {
                         if (trueCoords.yPos >= yRange[0] && trueCoords.yPos <= yRange[1]) {
                             const copyPeople = photos
@@ -31,7 +44,7 @@ const TargetBox: React.FC<ITargetBoxProps> = ( {mouseCoords, relativeCoords, try
                                 type: "updateFound",
                                 data: copyPeople
                             }
-                            return dispatch(updatePerson)
+                             dispatch(updatePerson)
                         }
                     }
                     else {
@@ -46,12 +59,34 @@ const TargetBox: React.FC<ITargetBoxProps> = ( {mouseCoords, relativeCoords, try
             defineTargetChar()
         }
 
+        const getCharPic = (name: string): string => {
+            switch (name) {
+                default:
+                    return ""
+                case "Waldo":
+                    return Waldo
+                case "Thoreau":
+                    return Thoreau
+                case "Whitman":
+                    return Whitman
+            }
+        }
+
         return (
             <S.Dropdown>
+                <p className={"dropDownHeader"}>Find Us:</p>
                 <ul>
-                    {photos.characters.map((person: IPerson) => <S.LiStyled key={person.name} found={person.found} onClick={() => checkSnapshotData(person.name)}>
-                        {person.name}
-                    </S.LiStyled>)}
+                    {photos.characters.map((person: IPerson) => {
+                        return (
+                            <S.LiStyled key={person.name}
+                                        found={person.found}
+                                        onClick={() => checkSnapshotData(person.name)}
+                            >
+                                {person.name}
+                                <img src={getCharPic(person.name)} alt={person.name} />
+                            </S.LiStyled>
+                        )
+                    })}
                 </ul>
             </S.Dropdown>
         )
@@ -61,7 +96,6 @@ const TargetBox: React.FC<ITargetBoxProps> = ( {mouseCoords, relativeCoords, try
         <S.TargetBox coords={mouseCoords} >
             <div className={"reticle"}/>
             { dropMenu()}
-
         </S.TargetBox>
     )
 };
